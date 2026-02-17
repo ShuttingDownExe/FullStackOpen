7403eee7bf462e664a3d90839542804706125448
Rishith Kumar <rishithkumar830@gmail.com>
2026-02-17 18:54:50 +0530

    modify

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Persons = ({persons}) => {
   return (
@@ -42,12 +43,20 @@ const PersonForm = ({newName, newPhone, handlePersonTextChange, handlePhoneTextC
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
+
+    useEffect(() => {
+        console.log("Use Effect Triggered")
+        axios.get('http://localhost:3001/persons').then(
+            response => {
+                console.log("Getting data")
+                console.log(response.data)
+                setPersons(response.data)
+                console.log("Got data")
+            }
+        )
+    }, []);
 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
@@ -55,7 +64,7 @@ const App = () => {
 
   const onClick = (event) => {
     event.preventDefault()
-    const blank = newName === "" || newPhone === "" ? true : false;
+    const blank = newName === "" || newPhone === "";
     if (blank) {
       alert("Blank details!")
       return
