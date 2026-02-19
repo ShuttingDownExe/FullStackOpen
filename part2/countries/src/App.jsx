2cb2a013e1492100c69a973b6cb2e6a9b3a7bfc9
Rishith Kumar <rishithkumar830@gmail.com>
2026-02-19 08:03:22 +0530

    init

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+const SearchBar = () => {
+  return (
+    <input></input>
+  )
+}
+
+function App() {
+
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  )
+}
+
+export default App
