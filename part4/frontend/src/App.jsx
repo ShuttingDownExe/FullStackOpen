70da35f230f1e7bcbd83cc81c6cea77f968abb18
Rishith Kumar <rishithkumar830@gmail.com>
2026-04-03 13:57:24 +0000

    part 4

---
+++ part4/frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import {useState, useEffect} from 'react';
+
+const blogItem = ({title, author, url, likes}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+const App = () => {
+  
+  return(
+    <>
+      <p>Hello World</p>
+    </>
+  )
+}
+
+export default App
